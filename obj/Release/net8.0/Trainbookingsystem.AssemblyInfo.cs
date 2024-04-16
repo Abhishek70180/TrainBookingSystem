c3b917e6436dacd5797dea0ed48b7c9c3d5d7549
qdevelopergithub <43872892+qdevelopergithub@users.noreply.github.com>
2024-04-16 15:20:39 +0530

    nm

--- obj/Release/net8.0/Trainbookingsystem.AssemblyInfo.cs
+++ obj/Release/net8.0/Trainbookingsystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-Trainbookingsystem-c3f0c83c-d15d-41c2-a01f-a912374a1648")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trainbookingsystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd58c49987c7e1201e00c1fdeafa98ae49abad1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trainbookingsystem")]
